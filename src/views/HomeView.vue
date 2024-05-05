72e83bbd7d440ea99dd8ce7f4b8956bd516648ad
LiuXinyang <771093658@qq.com>
2024-05-05 22:04:08 +0800

    vue first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div style="font-size: 18px">欢迎大家跟着武哥一起学习做毕业设计</div>
+    <div style="font-size: 18px; color: red; margin: 10px 0">点赞、投币、收藏、好评 + 关注，支持一波</div>
+    <div style="font-size: 18px">感谢大家</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+
+}
+</script>
